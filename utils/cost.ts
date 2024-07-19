52ac9620b84a66806e18d6e55df7ae4da1dffe5e
Wontae Chang <cwt629@naver.com>
2024-07-19 18:19:41 +0900

    feat: [240719] 강화비용 반환 유틸함수 구현

---
+++ utils/cost.ts
@@ -0,0 +1,46 @@
+export function getUpgradeCost(level: number, star: number): bigint {
+    switch (star) {
+        // 0~9성
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+            return BigInt(1000 + Math.pow(level, 3) * (star + 1) / 36);
+        // 10성
+        case 10:
+            return BigInt(1000 + Math.pow(level, 3) * Math.pow(star + 1, 2.7) / 571);
+        // 11성
+        case 11:
+            return BigInt(1000 + Math.pow(level, 3) * Math.pow(star + 1, 2.7) / 314);
+        // 12성
+        case 12:
+            return BigInt(1000 + Math.pow(level, 3) * Math.pow(star + 1, 2.7) / 214);
+        // 13성
+        case 13:
+            return BigInt(1000 + Math.pow(level, 3) * Math.pow(star + 1, 2.7) / 157);
+        // 14성
+        case 14:
+            return BigInt(1000 + Math.pow(level, 3) * Math.pow(star + 1, 2.7) / 107);
+        // 15-24성
+        case 15:
+        case 16:
+        case 17:
+        case 18:
+        case 19:
+        case 20:
+        case 21:
+        case 22:
+        case 23:
+        case 24:
+            return BigInt(1000 + Math.pow(level, 3) * Math.pow(star + 1, 2.7) / 200);
+
+        default:
+            return 0n;
+    }
+}
