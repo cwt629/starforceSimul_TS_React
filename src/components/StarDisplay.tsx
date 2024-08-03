a400a2a1de59acba03873acb0101fe54e74f9b50
Wontae Chang <cwt629@naver.com>
2024-08-03 18:10:49 +0900

    design: [240803] StarDisplay 컴포넌트에 별 이미지 적용

--- src/components/StarDisplay.tsx
+++ src/components/StarDisplay.tsx
@@ -1,30 +1,44 @@
 import { useSelector } from "react-redux";
 import { CurrentState } from "../type/state";
 
+import star_filled from '../images/star_filled.png';
+import star_empty from '../images/star_empty.png';
+import React from "react";
+
 function StarDisplay() {
     const currentStar: number = useSelector((state: CurrentState) => (state.currentStar));
     const maxStar: number = useSelector((state: CurrentState) => (state.maxStar));
 
-    const FULL_STAR = "★";
-    const EMPTY_STAR = "☆";
+    const FULL_STAR_PROPS: React.ImgHTMLAttributes<HTMLImageElement> = {
+        src: star_filled,
+        className: 'star'
+    };
+    const EMPTY_STAR_PROPS: React.ImgHTMLAttributes<HTMLImageElement> = {
+        src: star_empty,
+        className: 'star'
+    };
 
-    const generateStarDisplay = () => {
-        let stars: string[] = [];
+    const getStarInfo = (): boolean[][] => {
+        let stars: boolean[] = [];
         for (let i = 0; i < maxStar; i++) {
-            stars.push(i < currentStar ? FULL_STAR : EMPTY_STAR);
+            stars.push(i < currentStar);
         }
 
         // span 만들기
-        let spanElements: string[] = [];
+        let spanElements: boolean[][] = [];
         for (let i = 0; i < maxStar; i += 5) {
-            spanElements.push(stars.slice(i, i + 5).join(""));
+            spanElements.push(stars.slice(i, i + 5));
         }
 
         return spanElements;
     }
 
     return (<div className="star-div">
-        {generateStarDisplay().map((star, i) => (<span className="star" key={i}>{star}</span>))}
+        {getStarInfo().map((arr, i) => (<span className="stars" key={i}>{
+            arr.map((filled, index) => (filled ?
+                <img key={index} alt="star_filled" {...FULL_STAR_PROPS} />
+                : <img key={index} alt="star_empty" {...EMPTY_STAR_PROPS} />))
+        }</span>))}
     </div>)
 }
 
