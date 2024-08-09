0d1da38839398ea9f94873e4b5b224cc323ea67a
Wontae Chang <cwt629@naver.com>
2024-08-09 14:38:29 +0900

    fix: [240809] react-router-dom에 의해 빈 페이지가 출력되는 현상 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import RouterMain from './routers/RouterMain';
 import RouterLog from './routers/RouterLog';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<RouterMain />} />
-      <Route path='/log' element={<RouterLog />} />
-    </Routes>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path='/' element={<RouterMain />} />
+        <Route path='/log' element={<RouterLog />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
