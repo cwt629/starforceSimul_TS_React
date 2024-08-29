28ac14733c4d108b058ebfb03ad9f2a0f500ed45
Wontae Chang <cwt629@naver.com>
2024-08-29 14:57:54 +0900

    feat: [240829] 이벤트 관련 체크박스 UI 추가

--- src/components/Simulator.tsx
+++ src/components/Simulator.tsx
@@ -150,10 +150,27 @@ function Simulator() {
                                 </td>
                             </tr>
                         </tbody>
-
                     </table>
-
-
+                </div>
+            </div>
+            <div className="simul-options">
+                <span className="options-category">이벤트 옵션</span>
+                <div className="event-options">
+                    <div>
+                        <button className="btn btn-sm btn-outline-secondary">샤이닝 스타포스 적용</button>
+                    </div>
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" id="plusone_under_10" />
+                        <label className="form-check-label" htmlFor="plusone_under_10">10성 이하 강화 시 1+1</label>
+                    </div>
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" id="dc_30" />
+                        <label className="form-check-label" htmlFor="dc_30">강화 비용 30% 할인</label>
+                    </div>
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" id="success_5_10_15" />
+                        <label className="form-check-label" htmlFor="success_5_10_15">5, 10, 15성에서 강화 성공 100%</label>
+                    </div>
                 </div>
             </div>
             <div className="simul-ingame">
